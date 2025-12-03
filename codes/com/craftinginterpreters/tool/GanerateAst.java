ced5832a695b68f8e47fb4cfc12c521c39970070
ciso23041 <ciso23041@g.nihon-u.ac.jp>
2025-12-03 13:06:26 +0900

    GanerateAst

---
+++ codes/com/craftinginterpreters/tool/GanerateAst.java
@@ -0,0 +1,16 @@
+package codes.com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GanerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+    }
+        String outputDir = args[0];
+  }
+}
