15b1214fe51a439724d0f792e9dde52367a40579
ciso23041 <ciso23041@g.nihon-u.ac.jp>
2025-12-16 10:13:13 +0900

    add Perser.java

---
+++ codes/com/craftinginterpreters/lox/Perser.java
@@ -0,0 +1,77 @@
+package codes.com.craftinginterpreters.lox;
+
+import java.util.List;
+
+import static codes.com.craftinginterpreters.lox.TokenType.*;
+
+class Perser {
+    private final List<Token>tokens;
+    private int current = 0;
+
+    Perser(List<Token> tokens){
+        this.tokens = tokens;
+    }
+
+    private Expr expression(){
+        return equality();
+    }
+
+    private Expr equality(){
+        Expr expr = comparison();
+
+        while(match(BANG_EQUAL,EQUAL_EQUAL)){
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+            advance();
+            return true;
+            }
+        }
+    return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+
+
+
+
+}
