72e3406beaba27d75c8dba22d868feba19a79b9c
ciso23041 <ciso23041@g.nihon-u.ac.jp>
2026-01-13 11:41:32 +0900

    add Stmt and fix Lox,Parser,Interpreter

--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -20,7 +20,13 @@ public class GenerateAst {
             "Literal : Object value",
             "Unary : Token operator, Expr right"
         ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+          "Expression : Expr expression",
+          "Print      : Expr expression"
+        ));
   }
+
   private static void defineAst(
         String outputDir, String baseName, List<String> types)
         throws IOException{
